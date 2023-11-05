c7b470df3e43f34a0be44c0c27c7a210b7d9f35f
logkh22 <logkh22@ksu.edu>
2023-11-04 20:21:02 -0500

    Update
    Updated file names. GOOD TO FINISH WORK!

--- LevelSelect.gd
+++ LevelSelect.gd
@@ -3,8 +3,9 @@ extends CanvasLayer
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
+
 func _on_lvl_1btn_pressed():
-	get_tree().change_scene_to_file("res://LevelOne.tscn")
+	get_tree().change_scene_to_file("res://level1.tscn")
 
 
 func _on_lvl_2btn_pressed():
@@ -12,4 +13,4 @@ func _on_lvl_2btn_pressed():
 
 
 func _on_lvl_3btn_pressed():
-	get_tree().change_scene_to_file("res://world.tscn")
+	get_tree().change_scene_to_file("res://level3.tscn")
