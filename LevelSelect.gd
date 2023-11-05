110182b5046552b64f6c5fc013fecbe857c39206
Samuel Allred <jsallred@ksu.edu>
2023-11-04 19:56:01 -0500

    Rod is IN PROGRAM
    no longer appears only when running scene

--- LevelSelect.gd
+++ LevelSelect.gd
@@ -4,7 +4,7 @@ func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _on_lvl_1btn_pressed():
-	get_tree().change_scene_to_file("res://world.tscn")
+	get_tree().change_scene_to_file("res://LevelOne.tscn")
 
 
 func _on_lvl_2btn_pressed():
